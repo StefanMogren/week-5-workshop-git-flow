209ca596d0b7b7d7807a6e8bb4ba0b04e23bfb0a
DavidAkerlind <david.akerlind@icloud.com>
2025-01-31 11:27:27 +0100

    puching 3 functions

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ import apiHandler from "./apiHandler.js";
 import pagination from "./pagination.js";
 import characters from "./characters.js";
 
-function paginationSetup() {
-    const prevRef = document.querySelector('#prevPageBtn');
-    const nextRef = document.querySelector('#nextPageBtn');
+pageSetup();
 
-    prevRef.addEventListener('click', () => {
+function paginationSetup() {
+    const prevRef = document.querySelector("#prevPageBtn");
+    const nextRef = document.querySelector("#nextPageBtn");
+
+    prevRef.addEventListener("click", () => {
         pagination.previousPage();
         renderCharacters();
     });
-    
-    nextRef.addEventListener('click', () => {
+
+    nextRef.addEventListener("click", () => {
         pagination.nextPage();
         renderCharacters();
     });
-pageSetup();
-
+}
 function pageSetup() {
     fetchCharacters();
     paginationSetup();
